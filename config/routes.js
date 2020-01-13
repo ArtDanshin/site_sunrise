23f07759f19bc0e901f26f12071aa04495dabe57
Artem Danshin <silvir007@gmail.com>
2020-01-13 23:37:33 +0300

    Добавлен черновой вариант страницы категории галереи

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 const root   = require('../controllers/root');
+const gallery  = require('../controllers/gallery');
 const lod    = require('../controllers/lod');
 const shana  = require('../controllers/shana');
 
@@ -10,10 +11,12 @@ module.exports = app => {
   app.get('/gb', root.gb);
   app.get('/feedback', root.feedback);
 
+  app.get('/gallery/:category/', gallery.category);
+  app.get('/gallery/:category/:image', gallery.image);
+
   app.get('/lod', lod.info);
   app.get('/lod/articles', lod.articles);
   app.get('/lod/worldmap', lod.worldmap);
-  app.get('/lod/gallery', lod.gallery);
   app.get('/lod/video', lod.video);
   app.get('/lod/music', lod.music);
   app.get('/lod/faq', lod.faq);
