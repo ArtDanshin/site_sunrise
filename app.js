69503ee67648af86fdb39ad7fe442b105d18f704
Artem Danshin <silvir007@gmail.com>
2020-01-18 16:58:02 +0300

    Настроен сборщик Frontend'а и перезапуск Backend'а при изменении файлов

--- app.js
+++ app.js
@@ -38,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(settings.port, function() {
+  console.log(`Listening ${settings.port} port'`);
+});
