509bf97e13c319eec2de77cdb570c9ed4e26fb6a
Artem Danshin <silvir007@gmail.com>
2020-01-18 16:58:02 +0300

    Настроен сборщик Frontend'а и перезапуск Backend'а при изменении файлов

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { src, dest, parallel, watch } = require('gulp');
+const stylus = require('gulp-stylus');
+const nodemon = require('gulp-nodemon');
+
+function css() {
+  return src('assets/stylesheets/old/*.css')
+    .pipe(dest('public/assets/stylesheets'))
+}
+
+function styl() {
+  return src('assets/stylesheets/*.styl')
+    .pipe(stylus())
+    .pipe(dest('public/assets/stylesheets'))
+}
+
+exports.css = css;
+exports.stylus = stylus;
+
+exports.build = parallel(css, styl);
+exports.watch = function(cb) {
+  nodemon({
+    script: 'app.js',
+    ext: 'js pug json',
+    watch: [
+      "controllers/",
+      "db/",
+      "scraping/",
+      "utils/",
+      "views/"
+    ],
+    done: cb
+  });
+
+  watch('assets/stylesheets/old/*.css', css);
+  watch('assets/stylesheets/*.styl', styl);
+};
