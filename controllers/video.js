94141cbabe6ac35d3167706f9b50ea870780fb6d
Artem Danshin <silvir007@gmail.com>
2020-01-19 22:58:29 +0300

    Создана страница видео категорий

--- controllers/video.js
+++ controllers/video.js
@@ -1,5 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+
 exports.category = function(req, res) {
-  res.render('video/category', {
-    pageTitle: ''
-  });
+  const videosInfo = getVideosInfo();
+  const category = videosInfo.categories.find(cat => cat.slug === req.params.category);
+
+  if (category) {
+    const videos = videosInfo.videos.filter(video => video.category.includes(req.params.category));
+
+    if (videos.length) {
+      res.render('video/category', {
+        pageTitle: category.title,
+        videos
+      });
+    } else {
+      res.render('under_construction', {
+        pageTitle: category.title
+      });
+    }
+  } else {
+    res.status(404).end()
+  }
 };
+
+function getVideosInfo() {
+  return JSON.parse(fs.readFileSync(path.join(appRoot, 'db', 'video.json'), 'utf8'));
+}
