1d2202a28d3e7b42ffb465e62b748a116a02eb7b
Artem Danshin <silvir007@gmail.com>
2020-02-09 13:20:11 +0300

    Объединил списочные страницы под единый шаблон

--- controllers/files.js
+++ controllers/files.js
@@ -1,72 +1,33 @@
 const path = require('path');
 const fs = require('fs');
 
+const { renderCategory, renderDetail } = require('../helpers/listing_pages');
+
 exports.category = function(req, res) {
   const filesInfo = getFilesInfo();
   const category = filesInfo.categories.find(cat => cat.slug === req.params.category);
   const FILES_PER_PAGE = 10;
 
-  if (category) {
-    const pageNumber = req.params.page && parseInt(req.params.page);
-
-    if (pageNumber) {
-      const files = filesInfo.files.filter(file => file.category.includes(req.params.category));
-      const totalFiles = files.length;
-
-      if (totalFiles) {
-        const numberOfFirstFileOnPage = (pageNumber - 1) ? FILES_PER_PAGE * (pageNumber - 1): 0;
-
-        if (files[numberOfFirstFileOnPage]) {
-          const urlToCategory = `/files/${req.params.category}/`;
-
-          res.render('files/category', {
-            pageTitle: category.title,
-            currentPage: pageNumber,
-            filesPerPage: FILES_PER_PAGE,
-            totalFiles,
-            urlToCategory,
-            files: files.splice(numberOfFirstFileOnPage, FILES_PER_PAGE)
-          });
-        } else {
-          res.status(404).end()
-        }
-      } else {
-        res.render('under_construction', {
-          pageTitle: category.title
-        });
-      }
-    } else {
-      let url = req.originalUrl;
-      if (url.substr(-1) != '/') url += '/';
-
-      res.redirect(url + '1');
-    }
-  } else {
-    res.status(404).end()
-  }
+  renderCategory({
+    req,
+    res,
+    category,
+    items: filesInfo.files,
+    itemsPerPage: FILES_PER_PAGE,
+    type: 'files'
+  });
 };
 
 exports.detail = function(req, res) {
   const filesInfo = getFilesInfo();
   const file = filesInfo.files.find(file => file.slug === req.params.fileSlug);
 
-  if (file) {
-    if (file.mainBody) {
-      const category = filesInfo.categories.find(cat => cat.slug === file.category[0]);
-
-      res.render('files/detail', {
-        pageTitle: file.title,
-        file,
-        category
-      });
-    } else {
-      res.render('under_construction', {
-        pageTitle: file.title
-      });
-    }
-  } else {
-    res.status(404).end()
-  }
+  renderDetail({
+    res,
+    item: file,
+    categories: filesInfo.categories,
+    type: 'files'
+  });
 };
 
 function getFilesInfo() {
--- controllers/gallery.js
+++ controllers/gallery.js
@@ -1,49 +1,21 @@
 const path = require('path');
 const fs = require('fs');
 
+const { renderCategory } = require('../helpers/listing_pages');
+
 exports.category = function(req, res) {
   const gallery = getGalleriesInfo();
   const category = gallery.categories.find(cat => cat.slug === req.params.category);
   const IMAGES_PER_PAGE = 16;
 
-  if (category) {
-    const pageNumber = req.params.page && parseInt(req.params.page);
-
-    if (pageNumber) {
-      const images = gallery.images.filter(image => image.category.includes(req.params.category));
-      const totalImages = images.length;
-
-      if (totalImages) {
-        const numberOfFirstImageOnPage = (pageNumber - 1) ? IMAGES_PER_PAGE * (pageNumber - 1): 0;
-
-        if (images[numberOfFirstImageOnPage]) {
-          const urlToCategory = `/gallery/${req.params.category}/1`;
-
-          res.render('gallery/category', {
-            pageTitle: category.title,
-            currentPage: pageNumber,
-            imagesPerPage: IMAGES_PER_PAGE,
-            totalImages,
-            urlToCategory,
-            images: images.splice(numberOfFirstImageOnPage, IMAGES_PER_PAGE)
-          });
-        } else {
-          res.status(404).end()
-        }
-      } else {
-        res.render('under_construction', {
-          pageTitle: category.title
-        });
-      }
-    } else {
-      let url = req.originalUrl;
-      if (url.substr(-1) != '/') url += '/';
-
-      res.redirect(url + '1');
-    }
-  } else {
-    res.status(404).end()
-  }
+  renderCategory({
+    req,
+    res,
+    category,
+    items: gallery.images,
+    itemsPerPage: IMAGES_PER_PAGE,
+    type: 'gallery'
+  });
 };
 
 exports.image = function(req, res) {
