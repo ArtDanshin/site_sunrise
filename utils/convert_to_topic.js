2d93d828fc3e6bef47f1985433156f71c4fe4584
Artem Danshin <silvir007@gmail.com>
2020-02-21 23:18:25 +0300

    Переименованы картинки в соответствии с новыми slug

--- utils/convert_to_topic.js
+++ utils/convert_to_topic.js
@@ -26,7 +26,7 @@ async function processDb(dbName, propTopics, newType) {
   }
 
   const oldDb = await getDbFile(dbName);
-  const processedTopics = processTopics(oldDb[propTopics] || oldDb, newType, topics.length);
+  const processedTopics = await processTopics(oldDb[propTopics] || oldDb, newType, topics.length);
   topics = topics.concat(processedTopics);
 
 
@@ -54,49 +54,52 @@ async function writeDbFile(type, data) {
   await writer(path.join(__dirname, '..', 'db', `${type}.json`), JSON.stringify(data));
 }
 
-function processTopics(topics, finalType, beginIndex) {
-  return topics.map((topic, index) => ({
-    type: finalType,
-    title: topic.title,
-    slug: topic.slug
+async function processTopics(topics, finalType, beginIndex) {
+  const finalTopics = await Promise.all(topics.map(async (topic, index) => {
+    const slug = topic.slug
       ? formatSlug(topic.slug, beginIndex + index)
-      : `${beginIndex + index}-${finalType}`,
-    category: topic.category
-      ? topic.category.map(categorySlug => snakeToKebab(categorySlug))
-      : null,
-    views: topic.views,
-    author: topic.author,
-    rating: typeof topic.rating === 'number'
-      ? {
-        score: topic.rating,
-        votes: topic.rating > 0 ? 1 : 0
-      }
-      : topic.rating,
-    publishDate: topic.publishDate,
-    forTypeFile: topic.fileSize
-      ? {
-        fileSize: topic.fileSize
-      }
-      : null,
-    forTypeImage: topic.originImageInfo || topic.thumbImageInfo
-      ? {
-        originImageInfo: topic.originImageInfo,
-        thumbImageInfo: topic.thumbImageInfo
-      }
-      : null,
-    previewBody: topic.previewBody || null,
-    mainBody: topic.mainBody || null,
-    comments: Number.isInteger(topic.comments)
-      ? numberToEmptyArray(topic.comments)
-      : topic.comments.map(comment => (comment.body)
+      : `${beginIndex + index}-${finalType}`;
+
+    return {
+      type: finalType,
+      title: topic.title,
+      slug,
+      category: topic.category
+        ? topic.category.map(categorySlug => snakeToKebab(categorySlug))
+        : null,
+      views: topic.views,
+      author: topic.author,
+      rating: typeof topic.rating === 'number'
         ? {
-          body: comment.body,
-          date: comment.date,
-          userInfo: comment.userInfo
+          score: topic.rating,
+          votes: topic.rating > 0 ? 1 : 0
         }
-        : {}
-      )
-  }))
+        : topic.rating,
+      publishDate: topic.publishDate,
+      forTypeFile: topic.fileSize
+        ? {
+          fileSize: topic.fileSize
+        }
+        : null,
+      forTypeImage: topic.originImageInfo || topic.thumbImageInfo
+        ? await imageProcess(topic.originImageInfo, topic.thumbImageInfo, slug)
+        : null,
+      previewBody: topic.previewBody || null,
+      mainBody: topic.mainBody || null,
+      comments: Number.isInteger(topic.comments)
+        ? numberToEmptyArray(topic.comments)
+        : topic.comments.map(comment => (comment.body)
+          ? {
+            body: comment.body,
+            date: comment.date,
+            userInfo: comment.userInfo
+          }
+          : {}
+        )
+    };
+  }));
+
+  return finalTopics;
 }
 
 function processCategories(categories) {
@@ -125,4 +128,39 @@ function numberToEmptyArray(num) {
   }
 
   return resultArray;
+}
+
+async function imageProcess(originImageInfo, thumbImageInfo, slug) {
+  const rename = util.promisify(fs.rename);
+
+  const pathToImageFolder = path.join(__dirname, '..', 'public', 'images', 'gallery');
+  const pathToThumbFolder = path.join(pathToImageFolder, 'thumb');
+  const format = originImageInfo.format;
+  const newFilename = `${slug}.${format}`;
+
+  await rename(
+    path.join(pathToImageFolder, originImageInfo.filename),
+    path.join(pathToImageFolder, newFilename)
+  );
+  await rename(
+    path.join(pathToThumbFolder, thumbImageInfo.filename),
+    path.join(pathToThumbFolder, newFilename)
+  );
+
+  return {
+    originImageInfo: {
+      filename: newFilename,
+      size: originImageInfo.size,
+      height: originImageInfo.height,
+      width: originImageInfo.width,
+      format
+    },
+    thumbImageInfo: {
+      filename: newFilename,
+      size: thumbImageInfo.size,
+      height: thumbImageInfo.height,
+      width: thumbImageInfo.width,
+      format
+    }
+  }
 }
