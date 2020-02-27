20d82c7eb1574001fa0adf06885a66aae1f7bfd8
Artem Danshin <silvir007@gmail.com>
2020-02-27 22:56:21 +0300

    Middlewares вынесены отдельным слоем. Почищены лишние обработчики. Добавлена favicon

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const fs = require('fs');
-const createError = require('http-errors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 const app = express();
 
+const middlewares = fs.readdirSync(path.join(__dirname, 'app', 'middlewares')).sort();
 const settings = JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'settings.json'), 'utf8'));
 global.appRoot = path.resolve(__dirname);
 
@@ -14,28 +12,10 @@ global.appRoot = path.resolve(__dirname);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'app', 'views'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 require('./config/routes.js')(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+middlewares.forEach(middleware => {
+  app.use(require('./app/middlewares/' + middleware));
 });
 
 app.listen(settings.port, function() {
